6a7464be144b86b5ce32b9f6d9429d7197a79acc
Serge Kislak <kislak7@gmail.com>
2017-02-03 16:46:09 +0000

    Add parser, add exp

--- main.rb
+++ main.rb
@@ -2,12 +2,59 @@ require 'rubygems'
 require 'mechanize'
 require 'pry'
 
-a = Mechanize.new { |agent|
+agent = Mechanize.new { |agent|
   agent.user_agent_alias = 'Mac Safari'
 }
 
-a.get('http://realt.by') do |page|
- # ikit = IMGKit.new(page.body)	
- # file = ikit.to_file('image.png')
-  binding.pry
+references = %w(
+  971183
+  444301
+)
+
+KEYS = [
+  "Комнат всего/разд.",
+  "Этаж / этажность",
+  "Тип дома",
+  "Площадь общая/жилая/кухня",
+  "Планировка",
+  "Высота потолков",
+  "Год постройки",
+  "Сан/узел",
+  "Балкон",
+  "Ремонт",
+  "Дополнительно",
+  
+   "Область",
+   "Населенный пункт",
+   "Адрес",
+   "Район город",
+
+   "Условия продажи",
+   "Собственность",
+   "Ориентировочная стоимость эквивалентна",
+  
+   "Дата обновления",
+   "Телефоны",
+   "Агентство"
+]
+
+def get(agent, ref)
+  res = {}
+  agent.get("http://realt.by/sale/flats/object/#{ref}/") do |page|
+    KEYS.each do |key|
+      item = page.at("td:contains('#{key}')")
+      value = item&.next&.next&.text
+      res[key] = value 
+    end
+  end
+  res
 end
+
+references.each do |ref|
+  Pry::ColorPrinter.pp( get(agent, ref) )
+end
+  
+
+
+
+
